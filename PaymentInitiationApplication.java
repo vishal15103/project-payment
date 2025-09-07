26295f98a2a672fd3bb226fe27c2364f6cd98567
vishal15103 <121659782+vishal15103@users.noreply.github.com>
2025-09-07 21:14:47 +0530

    Create PaymentInitiationApplication.java

---
+++ PaymentInitiationApplication.java
@@ -0,0 +1,21 @@
+package com.example.paymentinitiation;
+
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+
+public class PaymentInitiationApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PaymentInitiationApplication.class, args);
+
+    }
+
+}
